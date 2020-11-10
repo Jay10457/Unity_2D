9218bdc9602f499238091f56001a4866990205b5
Jay10457 <71687778+Jay10457@users.noreply.github.com>
2020-11-10 16:05:49 +0800

    final

---
+++ New Unity Project/Assets/新增資料夾/LevelM.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelM : MonoBehaviour
+{
+   public void Next()
+    {
+
+
+
+    }
+    public void backmenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    public void quitgame()
+    {
+
+    }
+}
--- New Unity Project/Assets/新增資料夾/player.cs
+++ New Unity Project/Assets/新增資料夾/player.cs
@@ -1,13 +1,28 @@
-﻿
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+
     public GameObject final;
+    public Text textcount;
+    int count;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("touched");
-        final.SetActive(true);
+        if (collision.name == "teleport") 
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "gem")
+        {
+            Destroy(collision.gameObject);
+
+            count++;
+            textcount.text = "寶石:" + count;
+        }
+
+
+
     }
 }
